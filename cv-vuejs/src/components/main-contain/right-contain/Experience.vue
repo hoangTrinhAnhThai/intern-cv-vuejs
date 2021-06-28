746fc3e8f7015608a20112cfc07e2a6e9de32e6d
hoangtrinhanhthai <hoangtrinhanhthai@gmail.com>
2021-06-28 10:49:47 +0700

    + [main-contain] add right- experience component

---
+++ cv-vuejs/src/components/main-contain/right-contain/Experience.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="experience">
+    <h2>Experience</h2>
+    <hr />
+    <ul>
+      <li v-for="i in experiences" :key="i.id">
+        <h5>{{ i.name }}</h5>
+        <h6>{{ i.period }}</h6>
+        <p>{{ i.content }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["experiences"],
+};
+</script>
+
+<style>
+</style>
