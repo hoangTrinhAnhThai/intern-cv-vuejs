4b4f737390f799d80c87bd31b1082527e547c83a
hoangtrinhanhthai <hoangtrinhanhthai@gmail.com>
2021-06-28 10:48:58 +0700

    + [main-contain] add left - language

---
+++ cv-vuejs/src/components/main-contain/left-contain/Language.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="language">
+    <h2>Language</h2>
+    <hr />
+    <ul>
+      <li v-for="i in languages" :key="i.id">{{ i.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "language",
+  props: ["languages"],
+};
+</script>
+
+<style>
+</style>
