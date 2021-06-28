95ae0e88d6949a015872ad4d9605ac47173680a7
hoangtrinhanhthai <hoangtrinhanhthai@gmail.com>
2021-06-28 10:49:36 +0700

    + [main-contain] add right- education component

---
+++ cv-vuejs/src/components/main-contain/right-contain/Education.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="education">
+    <h2>Education</h2>
+    <hr />
+    <ul>
+      <li v-for="i in educations" :key="i.id">
+        <h5><i class="bx bx-book"></i> {{ i.name }}</h5>
+        <h6>{{ i.period }}</h6>
+        <p>{{ i.content }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["educations"],
+};
+</script>
+
+<style>
+</style>
