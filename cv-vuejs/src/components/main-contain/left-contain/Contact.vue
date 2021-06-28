0b6781082f601e92e75013c73ce0c758514820b2
hoangtrinhanhthai <hoangtrinhanhthai@gmail.com>
2021-06-28 10:48:33 +0700

    + [main-contain] add left - contact

---
+++ cv-vuejs/src/components/main-contain/left-contain/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="contact">
+    <h2>Contact</h2>
+    <hr />
+    <div class="phone">
+      <span><i class="bx bx-phone"></i> {{ contact.phone }}</span>
+    </div>
+    <div class="email">
+      <span><i class="bx bx-envelope"></i> {{ contact.email }}</span>
+    </div>
+    <div class="address">
+      <span><i class="bx bx-home"></i> {{ contact.address }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["contact"],
+};
+</script>
+
+<style>
+</style>
