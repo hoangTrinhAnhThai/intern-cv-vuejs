1f842ebda1b0c351bcf196369be8b7aa32a7935f
hoangtrinhanhthai <hoangtrinhanhthai@gmail.com>
2021-06-28 10:47:42 +0700

    + [main-contain] add left - achievement

---
+++ cv-vuejs/src/components/main-contain/left-contain/Achievement.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="achievement">
+    <h2>Achievements</h2>
+    <hr />
+    <p v-for="i in achivements" :key="i.id">{{ i.content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["achivements"],
+};
+</script>
+
+<style>
+</style>
