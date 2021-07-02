bc9c7851711aa72fbd6c178a143da682a9e0a7da
hoangtrinhanhthai <hoangtrinhanhthai@gmail.com>
2021-07-02 11:05:11 +0700

    + [api] install axios

--- cv-vuejs/src/axios.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-// axios.defaults.baseURL = 'https://60dd418f878c890017fa274b.mockapi.io/api/cv/'
-axios.defaults.baseURL = "https://cv-php-api.herokuapp.com/"
-// axios.defaults.headers.common['Authorization'];
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
